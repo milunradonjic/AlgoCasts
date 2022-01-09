c8d014da1503f3219c12cdca6f13621dfbc1ee48
milunradonjic <milun.radonjic95@gmail.com>
2022-01-09 18:59:33 +0100

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let maxCount = 0;
+  let max = '';
+
+  for (let c of str) {
+    charMap[c] = charMap[c] + 1 || 1;
+    if (charMap[c] > maxCount) {
+      maxCount = charMap[c];
+      max = c;
+    }
+  }
+
+  return max;
+}
 
 module.exports = maxChar;
