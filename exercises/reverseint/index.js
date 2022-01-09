3f6d73f117470161d1bc9d321dc8950a961ca7ae
milunradonjic <milun.radonjic95@gmail.com>
2022-01-09 18:41:32 +0100

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const sign = n < 0 ? -1 : 1; // Math.sign(n);
+  const revN = [...(n + '')].reverse().join('');
+  return sign * parseInt(revN);
+}
 
 module.exports = reverseInt;
