fb1bf4f04832498a1d25f4e177b67da444f84e3b
milunradonjic <milun.radonjic95@gmail.com>
2022-01-09 20:37:36 +0100

    array chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // 1. solution
+  const res = [];
+
+  let i = 0;
+  let newArr = [];
+
+  for (let x of array) {
+    newArr.push(x);
+    i++;
+    if (i == size) {
+      res.push(newArr);
+      newArr = [];
+      i = 0;
+    }
+  }
+
+  if (newArr.length > 0) {
+    res.push(newArr);
+  }
+
+  return res;
+
+  // 2. solution
+  // const chunked = [];
+  // let index = 0;
+
+  // while (index < array.length) {
+  //   chunked.push(array.slice(index, index + size));
+  //   index += size;
+  // }
+
+  // return chunked;
+}
 
 module.exports = chunk;
