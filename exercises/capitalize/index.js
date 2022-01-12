d1415e9a96a7807ad937637b9ea2e7639283ae82
milunradonjic <milun.radonjic95@gmail.com>
2022-01-12 09:47:45 +0100

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,13 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const words = str.split(' ');
+  const capitalizedString = words.map(word => 
+      word.charAt(0).toUpperCase() + word.slice(1)
+    ).join(' ');
+
+  return capitalizedString;
+}
 
 module.exports = capitalize;
