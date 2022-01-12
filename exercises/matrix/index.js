b6d76f602744a501db18e2419d24d8d29ef1faad
milunradonjic <milun.radonjic95@gmail.com>
2022-01-12 13:00:18 +0100

    matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,71 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let x = 1;
+
+  let arr = [];  
+
+  for (let i = 0; i < n; i++) {
+    let innerArr = []
+    for (let j = 0; j < n; j++) {
+      innerArr.push(0);
+    }
+    arr.push(innerArr);
+  }
+
+  let curNum = 1;
+
+  let startRow = 0;
+  let endCol = n - 1;
+  let endRow = n - 1;
+  let startCol = 0;
+
+  const finalNum = n*n;
+
+  /**
+   * 0 - left
+   * 1 - down
+   * 2 - right
+   * 3 - up
+   */
+  let i = 0;
+  
+  while (curNum <= finalNum) {
+
+    const dir = i % 4;
+    i++;
+
+    if (dir === 0) {
+      for (let j = startCol; j <= endCol; j++) {
+        arr[startRow][j] = curNum;
+        curNum++;
+      }
+      startRow++;
+    } else if (dir === 1) {
+      for (let j = startRow; j <= endRow; j++) {
+        arr[j][endCol] = curNum;
+        curNum++;
+      }
+      endCol--;
+    } else if (dir === 2) {
+      for (let j = endCol; j >= startCol; j--) {
+        arr[endRow][j] = curNum;
+        curNum++;
+      }
+      endRow--;
+    } else {
+      for (let j = endRow; j >= startRow; j--) {
+        arr[j][startCol] = curNum;
+        curNum++;
+      }
+      startCol++;
+    }
+  }
+
+  console.log("ARR : ", arr);
+
+  return arr;
+}
 
 module.exports = matrix;
