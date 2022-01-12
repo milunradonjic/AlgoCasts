7821f9739f8d121671c04531774a49d70edd2830
milunradonjic <milun.radonjic95@gmail.com>
2022-01-12 11:13:59 +0100

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // 1. solution
+  const vowels = 'aeiou';
+  let total = 0;
+
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) total++;
+  }
+
+  return total;
+
+  // 2. solution
+  // return [...str].reduce((numOfVowels, char) => 
+  // numOfVowels += vowels.includes(char.toLowerCase()) ? 1 : 0, 0);
+}
 
 module.exports = vowels;
